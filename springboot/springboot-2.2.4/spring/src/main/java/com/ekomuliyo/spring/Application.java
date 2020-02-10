c1973026b414020b805dfeb034faf03d07e0e1e7
eko <ekomuliyo.com>
2020-02-10 21:02:12 +0700

    dependency Injection 2

--- springboot/springboot-2.2.4/spring/src/main/java/com/ekomuliyo/spring/Application.java
+++ springboot/springboot-2.2.4/spring/src/main/java/com/ekomuliyo/spring/Application.java
@@ -11,8 +11,8 @@ public class Application {
 	public static void main(String[] args) {
 		ApplicationContext context =  SpringApplication.run(BelajarConfiguration.class, args);
 
-		DataBean dataBean = context.getBean(DataBean.class);
-		System.out.println(dataBean.getValue());
+		// DataBean dataBean = context.getBean(DataBean.class);
+		// System.out.println(dataBean.getValue());
 
 		SampleBean sampleBean = context.getBean(SampleBean.class);
 		System.out.println(sampleBean.getDataBean().getValue());
--- springboot/springboot-2.2.4/spring/src/main/java/com/ekomuliyo/spring/BelajarConfiguration.java
+++ springboot/springboot-2.2.4/spring/src/main/java/com/ekomuliyo/spring/BelajarConfiguration.java
@@ -3,8 +3,10 @@ package com.ekomuliyo.spring;
 import com.ekomuliyo.spring.model.DataBean;
 import com.ekomuliyo.spring.model.SampleBean;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
 
 /**
  * BelajarConfiguration
@@ -12,14 +14,20 @@ import org.springframework.context.annotation.Bean;
 @SpringBootApplication
 public class BelajarConfiguration {
 
-    @Bean
+    @Bean(name = "eko")
     public DataBean createDataBean(){
-        DataBean dataBean = new DataBean("Eko Muliyo");
+        DataBean dataBean = new DataBean("Eko");
+        return dataBean;
+    }
+
+    @Bean(name = "muliyo")
+    public DataBean createDataBean2(){
+        DataBean dataBean = new DataBean("Muliyo");
         return dataBean;
     }
 
     @Bean
-    public SampleBean createSampleBean(DataBean dataBean){
+    public SampleBean createSampleBean(@Qualifier("eko") DataBean dataBean){
         SampleBean sampleBean = new SampleBean(dataBean);
         return sampleBean;
     }
