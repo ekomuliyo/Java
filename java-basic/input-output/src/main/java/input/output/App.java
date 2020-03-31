7d7bff6fb05daaf058a890d17f3b27c4aa8f2637
eko muliyo <ekomuliyo@gmail.com>
2020-03-31 22:56:07 +0700

    clean

--- java-basic/input-output/src/main/java/input/output/App.java
+++ java-basic/input-output/src/main/java/input/output/App.java
@@ -5,8 +5,6 @@ package input.output;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Scanner;
-
 public class App {
 
     public static void main(String[] args) {
