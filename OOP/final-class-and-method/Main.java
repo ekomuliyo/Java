2351dd7e440f464d6f19a5dbca5ae95da2ea4fad
eko muliyo <ekomuliyo@gmail.com>
2020-11-22 21:37:37 +0700

    final-class-and-method

---
+++ OOP/final-class-and-method/Main.java
@@ -0,0 +1,28 @@
+public class Main {
+    public static void main(String[] args) {
+        
+    }
+}
+
+class SocialMedia {
+    String name;
+}
+
+class Facebook extends SocialMedia {
+
+    final void login(String username, String password) {
+
+    }
+}
+
+class FakeFacebook extends Facebook {
+    // Error
+    // final void login(String username, String password) {
+
+    // }
+}
+
+// Error
+// class FakeFacebook extends Facebook {
+
+// }
