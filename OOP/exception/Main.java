2c27246cc12cf77d741fc7f778e73e5fbfd822d8
eko muliyo <ekomuliyo@gmail.com>
2020-12-14 22:14:25 +0700

    exception

---
+++ OOP/exception/Main.java
@@ -0,0 +1,56 @@
+public class Main {
+    public static void main(String[] args) {
+        
+        LoginRequest loginRequest = new LoginRequest("Eko", null);
+
+        try {
+            ValidationUtil.validate(loginRequest);
+            System.out.println("Data valid");
+        } catch (ValidationException | NullPointerException e) {
+            System.out.println("Pesan error : " +e.getMessage());
+        } finally {
+            System.out.println("Selalu dieksekusi");
+        }
+    }
+}
+
+class ValidationUtil {
+    
+    static void validate(LoginRequest loginRequest) throws ValidationException {
+        if (loginRequest.username() == null) {
+            throw new NullPointerException("Username is null");
+        } else if (loginRequest.username().isBlank()) {
+            throw new ValidationException("Username is blank");
+        } else if (loginRequest.password() == null) {
+            throw new NullPointerException("Password is null");
+        } else if (loginRequest.password().isBlank()) {
+            throw new ValidationException("Password is blank");
+        }
+    }
+}
+
+class ValidationException extends Throwable {
+    
+    ValidationException(String message) {
+        super(message);
+    }
+}
+
+record LoginRequest(String username, String password) { 
+    
+    // Constructor
+    LoginRequest {
+        System.out.println("membuat object LoginRequest");
+    }
+
+    // Overloading constructor 
+    LoginRequest(String username) {
+        this(username, "");
+    }
+
+    LoginRequest() {
+        this("", "");
+    }
+
+}
+
