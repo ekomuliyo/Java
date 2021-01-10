d11ab8f2924e5f04b481ee7d070efb1ae76bb5a3
eko <ekomuliyo@gmail.com>
2021-01-10 21:15:42 +0700

    string-builder

---
+++ standard-classes/string-builder/Main.java
@@ -0,0 +1,15 @@
+/**
+ * Main
+ */
+public class Main {
+    public static void main(String[] args) {
+        
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("Eko");
+        stringBuilder.append(" ");
+        stringBuilder.append("Muliyo");
+
+        String name = stringBuilder.toString();
+        System.out.println(name);
+    }
+}
