8e1eaf4df4570226b1bd58234c1c49611309ce73
eko <ekomuliyo@gmail.com>
2021-01-10 21:46:15 +0700

    math

---
+++ standard-classes/math/Main.java
@@ -0,0 +1,15 @@
+/**
+ * Main
+ */
+public class Main {
+    public static void main(String[] args) {
+        
+        var min = Math.min(10, 20);
+        System.out.println(min);
+
+        var max = Math.max(10, 20);
+        System.out.println(max);
+
+        System.out.println(Math.PI);
+    }
+}
