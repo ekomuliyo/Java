108bf6b6cfd656a6a25ebd914175cdf553d5a536
eko <ekomuliyo@gmail.com>
2021-01-11 09:15:15 +0700

    system

---
+++ standard-classes/system/Main.java
@@ -0,0 +1,17 @@
+/**
+ * Main
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(System.currentTimeMillis());
+        System.out.println(System.nanoTime());
+
+        System.out.println(System.getenv("HOMEPATH"));
+        
+        System.exit(0);
+        
+        System.out.println("hi");
+
+    }
+    
+}
