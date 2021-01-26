b4e95d38e8aed12f0369c4f150067c03ad6a6e43
eko <ekomuliyo@gmail.com>
2021-01-26 22:15:26 +0700

    enum-map

---
+++ collection/enum-map/Main.java
@@ -0,0 +1,25 @@
+import java.util.EnumMap;
+import java.util.Map;
+
+/**
+ * Main
+ */
+public class Main {
+
+    public static enum Level {
+        FREE, STANDARD, PREMIUM, VIP
+    }
+
+    public static void main(String[] args) {
+        
+        Map<Level, String> map = new EnumMap<Level, String>(Level.class);
+
+        map.put(Level.FREE, "Eko");
+        map.put(Level.STANDARD, "Muliyo");
+
+        for(var key: map.keySet()) {
+            System.out.println(map.get(key));
+        }
+    }
+    
+}
